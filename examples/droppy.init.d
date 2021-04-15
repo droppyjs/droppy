717105b57f370d14dd3117244582647c5a2dbdea
Mark Hughes <markwilliamhughes1@gmail.com>
2021-04-15 21:02:34 +1000

    feat: add status to init.d script

--- examples/droppy.init.d
+++ examples/droppy.init.d
@@ -10,9 +10,12 @@
 # Description:       droppy - github.com/droppyjs/droppy
 ### END INIT INFO
 
+DROPPY_CONFIG_DIR="/srv/droppy/config"
+DROPPY_FILES_DIR="/srv/droppy/files"
+
 PROCESS="droppy"
 RUNAS="droppy:droppy"
-CMD="/usr/bin/env droppy start -c \"/srv/droppy/config\" -f \"/srv/droppy/files\""
+CMD="/usr/bin/env droppy -- start -c '$DROPPY_CONFIG_DIR' -f '$DROPPY_FILES_DIR'"
 
 do_start() {
     start-stop-daemon --start --background -c $RUNAS --name $PROCESS --exec $CMD 
@@ -22,6 +25,15 @@ do_stop() {
     start-stop-daemon --stop --name $PROCESS
 }
 
+do_status() {
+    if pgrep -x "$PROCESS" >/dev/null
+    then
+        echo "$PROCESS is running"
+    else
+        echo "$PROCESS stopped"
+    fi
+}
+
 case "$1" in
   start)
     do_start
@@ -29,12 +41,15 @@ case "$1" in
   stop)
     do_stop
   ;;
+  status)
+    do_status
+  ;;
   restart)
     do_stop
     do_start
   ;;
   *)
-    echo "Usage: "$1" {start|stop|restart}"
+    echo "Usage: "$1" {start|stop|status|restart}"
     exit 1
   ;;
 esac
